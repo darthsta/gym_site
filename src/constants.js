2b17a13b50712d19c761a30e78b6d44c0fac3ea8
agoguen <andrew@gbtel.ca>
2025-04-18 18:02:39 -0400

    Adding contstants file for maintainability

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const BUSINESS_NAME = "Your Business Name Here";
+export const BUSINESS_DESCRIPTION = "Your Business Description Here";
+export const BUSINESS_ADDRESS = "123 Business St, City, State, Zip";
+export const TOMATO_RED = "#ff6347";
